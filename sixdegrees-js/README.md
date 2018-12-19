acfd9e7f20b042e3b0f8004bf71a587a7cdd404c
Thomas Bueno <Thomas.Bueno@elliemae.com>
2018-12-18 20:19:58 -0800

    finished interview question

---
+++ sixdegrees-js/README.md
@@ -0,0 +1,11 @@
+Degrees Of Separation Test
+====================
+
+# Prerequisites
+- NodeJs / 
+npm
+
+# Build & run test
+```shell
+$ npm install & npm test`
+```
---
+++ sixdegrees-js/test/FriendFinder.test.js
@@ -0,0 +1,26 @@
+import SocialNetworkServiceImpl from './SocialNetworkServiceImpl';
+import FriendFinder from '../src/FriendFinder';
+
+describe('Friend Finder Test', () => {
+
+  it('find relationship path bonus question test', () => {
+
+    const sns = new SocialNetworkServiceImpl();
+    sns.addFriend('Kevin', 'UserB');
+    sns.addFriend('Kevin', 'UserS');
+    sns.addFriend('UserB', 'UserC');
+    sns.addFriend('UserA', 'UserD');
+    sns.addFriend('UserX', 'UserC');
+    sns.addFriend('UserY', 'UserX');
+    sns.addFriend('Bacon', 'UserY');
+
+    const friendFinder = new FriendFinder(sns);
+
+    expect(friendFinder.findConnectionPath('Kevin', 'Bacon', 5))
+      .toEqual(['Kevin', 'UserB', 'UserC', 'UserX', 'UserY', 'Bacon']);
+
+    sns.addFriend('UserS', 'Bacon');
+    expect(friendFinder.findConnectionPath('Kevin', 'Bacon', 5))
+      .toEqual(['Kevin', 'UserS', 'Bacon']);
+  });
+});
